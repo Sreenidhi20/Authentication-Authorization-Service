47081def2324b9d0daf862df87d6de055e7d65fa
Sreenidhi <sreenidhibs04@gmail.com>
2026-08-15 17:20:23 +0530

    feat : add vercel FE endpoint to fix CORS Error.

--- auth-service/app/main.py
+++ auth-service/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import sql_health
 
@@ -6,6 +7,14 @@ app = FastAPI(
     title="Authentication-Authorization-Service",
     description="Stack: FastAPI + PostgreSQL + SQLAlchemy + JWT (python-jose) + passlib + Authlib (Google OAuth) + slowapi (rate limiting) Frontend: React + MUI",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173","https://authentication-authorization-servic.vercel.app/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
